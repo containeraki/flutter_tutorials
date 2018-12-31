a33e157b661714e7e300ea93b9d7b408b0454248
Akshat Sharma <er.sharmaakshat@gmail.com>
2018-12-31 12:31:14 +0400

    Flutter initial Boilerplate

---
+++ android/app/src/main/java/dev/akshat/akshatlearn1/MainActivity.java
@@ -0,0 +1,13 @@
+package dev.akshat.akshatlearn1;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
